bf8c0d5c9f33a67c123e649f5b344fd3a87ae000
Caner Uçar <caner.ucar@akinon.com>
2021-08-01 14:54:31 +0300

    Add update & delete & add photo

--- app.js
+++ app.js
@@ -1,31 +1,56 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override');
 const ejs = require('ejs');
-const path = require('path');
+const photoController = require('./controllers/photoControllers');
+const pageController = require('./controllers/pageController');
 
 const app = express();
 
-//TEMPLATE ENGINE
-app.set("view engine", "ejs");
+//connect DB
+mongoose
+  .connect(
+    'mongodb://localhost/pcat-test-db',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => {
+    console.log('DB CONNECTED!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-//MIDDLEWARE
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+
+//MIDDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(fileUpload());
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
 
 //ROUTES
-app.get('/', (req, res)=> {
-  //res.sendFile(path.resolve(__dirname, 'views/index.html'))
-  res.render('index');
-});
+app.get('/', photoController.getAllPhotos);
+app.get('/photos/:id', photoController.getPhoto);
+app.post('/photos', photoController.createPhoto);
+app.put('/photos/:id', photoController.updatePhoto);
+app.delete('/photos/:id', photoController.deletePhoto);
 
-app.get('/about', (req, res) => {
-  res.render('about');
-});
+app.get('/about', pageController.getAboutPage);
+app.get('/add', pageController.getAddPage);
+app.get('/photos/edit/:id', pageController.getEditPage);
 
-app.get('/add', (req, res) => {
-  res.render('add');
-});
-
-const port = 8000;
-
-app.listen(port, ()=> {
+const port = process.env.PORT || 8001;
+app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı..`);
-})
+});
